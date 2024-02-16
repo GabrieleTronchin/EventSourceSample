9257a52ac5b617a40ebc806696a2085891d0b125
Gabriele Tronchin <gabriele.tronchin@reti.it>
2024-02-16 16:38:53 +0100

    Add extension for minimal API

---
+++ src/EventSource.Application/ForecastModel.cs
@@ -0,0 +1,35 @@
+﻿using EventSource.Application.Account;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSource.Application;
+
+public static class ForecastModel
+{
+    public static void AddForecastModelEndpoint(this IEndpointRouteBuilder app)
+    {
+
+        var sampleTodos = new Todo[] {
+            new(1, "Walk the dog"),
+            new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+            new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+            new(4, "Clean the bathroom"),
+            new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+        };
+
+
+        app.MapGet("/", () => sampleTodos);
+        app.MapGet("/{id}", (int id) =>
+            sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+                ? Results.Ok(todo)
+                : Results.NotFound());
+
+    }
+
+}
